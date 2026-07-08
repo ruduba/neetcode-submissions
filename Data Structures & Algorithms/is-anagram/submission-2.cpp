5ff4682ffe4ffcacd29ab2bc90741de957fb238f
vatsala budur <114461494+ruduba@users.noreply.github.com>
2026-07-08 20:19:59 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size() || s.empty() || t.empty()) return false;
+        unordered_map<char, int> m1;
+        unordered_map<char, int> m2;
+
+        for(auto x: s){
+            m1[x]++;
+        }
+        for(auto x: t){
+            m2[x]++;
+        }
+
+        if(m1 == m2) return true;
+
+        return false;
+    }
+};
