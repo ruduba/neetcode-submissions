746796a83a65c024d1b186c081c6baf544aea95b
vatsala budur <114461494+ruduba@users.noreply.github.com>
2026-07-18 22:33:57 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        transform(s.begin(), s.end(), s.begin(), [&] (char c){
+            return tolower(c);
+        });
+        erase(s, ' ');
+        s.erase(remove_if(s.begin(), s.end(), [] (char c){
+            return !isalnum(c);
+        }), s.end());
+
+        int l = 0, r = s.size()-1;
+
+        while(l<r){
+            if(s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+};
